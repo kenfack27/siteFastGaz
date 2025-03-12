001e47e1ea0f5fc30b26f2a7f38a24c4ee92507a
DEVELOPPEUR JAVA <developpeuralvaro@gmail.com>
2025-03-12 12:17:14 +0100

    first commit

---
+++ android/app/src/main/java/com/tondomaine/gazlocated/MainActivity.java
@@ -0,0 +1,5 @@
+package com.tondomaine.gazlocated;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.tondomaine.gazlocated',
+  appName: 'FastGaz',
+  webDir: 'www'
+};
+
+export default config;
